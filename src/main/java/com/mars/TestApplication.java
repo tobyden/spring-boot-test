f4ab71fb3578e5c47038c86036dfdab016aacdfb
mars <mars@qq.com>
2021-05-29 20:15:39 +0800

    second commit

--- src/main/java/com/mars/TestApplication.java
+++ src/main/java/com/mars/TestApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class TestApplication {
 
 	public static void main(String[] args) {
+		//启动程序
 		SpringApplication.run(TestApplication.class, args);
 	}	
 }
